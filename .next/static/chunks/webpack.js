2dbdb0da412dc7a289b126040ecf73caefcec57c
jeff0518 <cycivs07@icloud.com>
2023-06-20 17:59:46 +0800

    feat: add 404

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b885e808c7a42fb7"; }
+/******/ 		__webpack_require__.h = function() { return "223e657e94b98819"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router";
 function ClientProjectsPage() {
+  const router = useRouter()
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientProjectid]",
+      query: { id: "jeff", clientProjectid: "projecta" },
+    });
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>A</button>
     </div>
   );
 }
